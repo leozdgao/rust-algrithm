f01b4248dd03e3ae5ee36cd7e6e03126bb8047dc
leozdgao <gzdleo0816@outlook.com>
2020-07-28 01:43:21 +0800

    feat: 15

---
+++ src/leetcode/15.rs
@@ -0,0 +1,13 @@
+//! 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+//! 
+//! 来源：力扣（LeetCode）
+//! 链接：https://leetcode-cn.com/problems/3sum
+//! 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    // 如果数组内元素小于 3 个，直接返回空
+    if (nums.len() < 3) {
+        return vec![];
+    }
+    
+
+}
